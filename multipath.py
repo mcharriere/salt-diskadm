62a9faaeb5f512c1393a804f113eb479dbfa3539
Matias Charriere <mch@frba.utn.edu.ar>
2018-02-01 15:58:28 -0300

    copied devmap module from saltstack as multipath module

---
+++ multipath.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+'''
+Multipath module
+'''
+from __future__ import absolute_import, print_function, unicode_literals
+import os.path
+
+
+def list():
+    '''
+    Multipath list
+
+    CLI Example:
+
+    .. code-block:: bash
+
+        salt '*' multipath.list
+    '''
+    cmd = 'multipath -l'
+    return __salt__['cmd.run'](cmd).splitlines()
+
+
+def flush(device):
+    '''
+    Multipath flush
+
+    CLI Example:
+
+    .. code-block:: bash
+
+        salt '*' multipath.flush mpath1
+    '''
+    if not os.path.exists(device):
+        return '{0} does not exist'.format(device)
+
+    cmd = 'multipath -f {0}'.format(device)
+    return __salt__['cmd.run'](cmd).splitlines()
+
